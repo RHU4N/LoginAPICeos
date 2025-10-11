0e34a5894ff3af2622bd56175f65ac5902b582a0
Vitor <vitorbastos195@gmail.com>
2025-10-11 16:15:10 -0300

    continuando swagger

--- src/interfaces/routes/AuthRoutes.js
+++ src/interfaces/routes/AuthRoutes.js
@@ -13,6 +13,85 @@ const tokenProvider = new JwtTokenProvider();
 const loginUseCase = new LoginUseCase(userUseCases, passwordHasher, tokenProvider);
 const authController = new AuthController(loginUseCase);
 
+/**
+ * @openapi
+ * /login:
+ *   post:
+ *     tags: [Auth]
+ *     summary: Executa login.
+ *     description: Recebe `email`,`senha` e realiza login.
+ *     requestBody:
+ *         required: true
+ *         content: 
+ *             application/json:
+ *                 schema:
+ *                     type: object
+ *                     properties:
+ *                         email:
+ *                            type: string
+ *                            example: jonh@email.com
+ *                         senha:
+ *                            type: string
+ *                            example: 12345
+ *     responses:
+ *       200:
+ *         description: Login executado com sucesso
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: hk1239k%ndsa
+ *       400:
+ *         description: Campos obrigatórios faltando
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                 - type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: Campos obrigatórios faltando
+ *                 - type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: Token não fornecido
+ *       401:
+ *         description: Usuário não encontrado
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                 - type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: Usuário não encontrado
+ *                 - type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: Senha inválida
+ *                 - type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: Token inválido
+ *       500:
+ *         description: Erro interno no servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Erro interno no servidor
+ */
 router.post('/login', (req, res) => authController.login(req, res));
 
 module.exports = router;
--- src/interfaces/routes/UserRoutes.js
+++ src/interfaces/routes/UserRoutes.js
@@ -23,9 +23,50 @@ const userController = new UserController(userUseCases);
  *   get:
  *     tags: [Users]
  *     summary: Listar usuários
+ *     description: Lista todos os usuários cadastrados
  *     responses:
  *       200:
  *         description: Lista de usuários
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items: 
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                   nome:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ *                   senha:
+ *                     type: string
+ *                   telefone:
+ *                     type: string
+ *                   assinante:
+ *                     type: boolean
+ *                   historico:
+ *                     type: object
+ *                     properties:
+ *                       tipo:
+ *                         type: string
+ *                       valores:
+ *                         type: string
+ *                       resultado:
+ *                         type: string
+ *                       data:
+ *                         type: date
+ *       500: 
+ *         description: Erro interno do servidor
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties: 
+ *                 error:
+ *                   type: string
+ *                   example: Erro interno do servidor
  */
 router.get('/', (req, res) => userController.getAll(req, res));
 /**
@@ -34,6 +75,7 @@ router.get('/', (req, res) => userController.getAll(req, res));
  *   post:
  *     tags: [Users]
  *     summary: Criar usuário
+ *     description: Recebe os parâmetros `"nome"`, `"email"`, `"senha"`, `"telefone"`, `"assinante"` e `"histórico"` e registra usuário. 
  *     requestBody:
  *       required: true
  *       content:
@@ -43,13 +85,86 @@ router.get('/', (req, res) => userController.getAll(req, res));
  *             properties:
  *               nome:
  *                 type: string
+ *                 example: John Doe
  *               email:
  *                 type: string
+ *                 example: john@email.com
  *               senha:
  *                 type: string
+ *                 example: 12345
+ *               telefone: 
+ *                 type: string
+ *                 example: 11111-1111
+ *               assinante:
+ *                 type: boolean
+ *                 example: true
+ *               historico:
+ *                 type: object
+ *                 properties:
+ *                   tipo:
+ *                     type: string
+ *                   valores: 
+ *                     type: string
+ *                   resultado:
+ *                     type: string
+ *                   data: 
+ *                     type: string
+ *                     format: date
  *     responses:
  *       201:
- *         description: Usuário criado
+ *         description: Cadastrado com sucesso
+ *         content: 
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 nome:
+ *                   type: string
+ *                   example: John Doe
+ *                 email:
+ *                   type: string
+ *                   example: john@email.com
+ *                 senha:
+ *                   type: string
+ *                   example: 12345
+ *                 telefone: 
+ *                   type: string
+ *                   example: 11111-1111
+ *                 assinante:
+ *                   type: boolean
+ *                   example: true
+ *                 historico:
+ *                   type: object
+ *                   properties:
+ *                     tipo:
+ *                       type: string
+ *                     valores: 
+ *                       type: string
+ *                     resultado:
+ *                       type: string
+ *                     data: 
+ *                       type: string
+ *                       format: date
+ *       400:
+ *         description: Campos obrigatórios faltando
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties: 
+ *                 error:
+ *                   type: string
+ *                   example: Campo "nome" é obrigatório
+ *       500: 
+ *         description: Erro interno do servidor
+ *         content: 
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties: 
+ *                 error:
+ *                   type: string
+ *                   example: Erro interno do servidor
  */
 router.post('/', (req, res) => userController.create(req, res));
 /**
@@ -58,20 +173,296 @@ router.post('/', (req, res) => userController.create(req, res));
  *   get:
  *     tags: [Users]
  *     summary: Buscar usuário por id
+ *     description: Realiza busca de usuário filtrada por `Id`.
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
+ *         description: Parâmetro de busca
  *         schema:
  *           type: string
  *     responses:
  *       200:
- *         description: Usuário
+ *         description: Usuário encontrado
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 nome:
+ *                   type: string
+ *                   example: John Doe
+ *                 email:
+ *                   type: string
+ *                   example: john@email.com
+ *                 telefone: 
+ *                   type: string
+ *                   example: 11111-1111
+ *                 assinante:
+ *                   type: boolean
+ *                   example: true
+ *                 historico:
+ *                   type: object
+ *                   properties:
+ *                     tipo:
+ *                       type: string
+ *                     valores: 
+ *                       type: string
+ *                     resultado:
+ *                       type: string
+ *                     data: 
+ *                       type: string
+ *                       format: date
+ *       404:
+ *         description: Usuário não encontrado
  */
 router.get('/:id', (req, res) => userController.getById(req, res));
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   put:
+ *     tags: [Users]
+ *     summary: Atualiza dados do usuário
+ *     description: Realiza busca de usuário filtrada por `Id` e altera os campos específicados.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Parâmetro de busca
+ *         schema:
+ *           type: string
+ *     requestBody: 
+ *         required: false
+ *         content:
+ *             application/json:
+ *                 schema:
+ *                     type: object
+ *                     properties:
+ *                         nome:
+ *                            type: string
+ *                            example: João Ninguém
+ *                         assinante:
+ *                            type: boolean
+ *                            example: false
+ *     responses:
+ *       200:
+ *         description: Usuário encontrado
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 nome:
+ *                   type: string
+ *                   example: João Ninguém
+ *                 email:
+ *                   type: string
+ *                   example: john@email.com
+ *                 telefone: 
+ *                   type: string
+ *                   example: 11111-1111
+ *                 assinante:
+ *                   type: boolean
+ *                   example: false
+ *                 historico:
+ *                   type: object
+ *                   properties:
+ *                     tipo:
+ *                       type: string
+ *                     valores: 
+ *                       type: string
+ *                     resultado:
+ *                       type: string
+ *                     data: 
+ *                       type: string
+ *                       format: date
+ *       400:
+ *         description: Campos obrigatórios faltando
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Campos obrigatórios faltando
+ *       404:
+ *         description: Usuário não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Usuário não encontrado
+ *       500:
+ *         description: Erro interno no servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Erro interno no servidor
+ */
 router.put('/:id', (req, res) => userController.update(req, res));
+
+/**
+ * @openapi
+ * /users/{id}:
+ *   delete:
+ *     tags: [Users]
+ *     summary: Deleta usuário
+ *     description: Realiza busca de usuário filtrada por `Id` e deleta o usuário.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Parâmetro de busca
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Excluído com sucesso
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Excluído com sucesso
+ *       404:
+ *         description: Usuário não encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Usuário não encontrado
+ *       500:
+ *         description: Erro interno no servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Erro interno no servidor
+ */
 router.delete('/:id', (req, res) => userController.delete(req, res));
+
+/**
+ * @openapi
+ * /historico:
+ *   post:
+ *     tags: [Users]
+ *     summary: Adiciona operação ao histórico do usuário
+ *     description: Operação matemática é adicionada ao histórico do usuário.
+ *     requestBody:
+ *         required: true
+ *         content: 
+ *             application/json:
+ *                 schema:
+ *                     type: object
+ *                     properties:
+ *                         tipo:
+ *                            type: string
+ *                         valores:
+ *                            type: string
+ *                         resultado:
+ *                            type: string
+ *     responses:
+ *       201:
+ *         description: Histórico salvo com sucesso
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Histórico salvo com sucesso
+ *       400:
+ *         description: Campos obrigatórios faltando
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Campos obrigatórios faltando
+ *       500:
+ *         description: Erro interno no servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Erro interno no servidor
+ */
 router.post('/historico', auth, (req, res) => userController.addHistorico(req, res));
+
+/**
+ * @openapi
+ * /historico:
+ *   get:
+ *     tags: [Users]
+ *     summary: Retorna histórico do usuário
+ *     description: Lista as operações matemáticas realizadas pelo usuário.
+ *     requestBody:
+ *         required: true
+ *         content: 
+ *             application/json:
+ *                 schema:
+ *                     type: object
+ *                     properties:
+ *                         id:
+ *                            type: string
+ *     responses:
+ *       201:
+ *         description: Histórico salvo com sucesso
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: object
+ *               properties:
+ *                 tipo:
+ *                   type: string
+ *                 valores:
+ *                   type: string
+ *                 resultado:
+ *                   type: string
+ *       400:
+ *         description: Campos obrigatórios faltando
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Campos obrigatórios faltando
+ *       500:
+ *         description: Erro interno no servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error: 
+ *                   type: string
+ *                   example: Erro interno no servidor
+ */
 router.get('/historico', auth, (req, res) => userController.getHistorico(req, res));
 
 module.exports = router;
